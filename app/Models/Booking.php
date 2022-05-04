0565eef0bca75150bb8077381c485bd723d1ed1b
Apryms <madonaapry@gmail.com>
2022-05-04 21:30:02 +0700

    make relationship. next view

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Booking extends Model
 {
@@ -20,4 +21,8 @@ class Booking extends Model
     ];
 
     protected $hidden = [];
+
+    public function User(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
